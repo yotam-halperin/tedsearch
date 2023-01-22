11fb9924840310e8be9a8e8faff7634560d733cb
Yotam Halperin <yotamhalperin7@gmail.com>
2023-01-22 09:37:43 +0000

    .

---
+++ describer.groovy
@@ -0,0 +1,46 @@
+pipeline {
+    agent any
+    
+    tools {
+        terraform 'tf'
+    }
+
+    stages {
+
+        // clone the git repository
+        stage('clone'){
+            steps {
+                deleteDir()
+                checkout scm
+            }
+        }
+
+// km
+        stage('check envs'){
+            steps{
+                script{
+                    env.result=sh(returnStdout: true, script: 'bash describer.sh')
+                }
+            }
+        }
+
+    }
+    post {
+
+        always {  
+                mail bcc: '',
+                            body: """<b>ENVIROMENTS</b>
+                                <br> envs: <br> ${env.result}
+                                <br>Project: ${env.JOB_NAME} 
+                                <br>Build Number: ${env.BUILD_NUMBER} 
+                                <br> URL de build: ${env.BUILD_URL}""",
+                            cc: '',
+                            charset: 'UTF-8',
+                            from: '',
+                            mimeType: 'text/html',
+                            replyTo: '', 
+                            subject: "calc enviroments",
+                            to: sh(returnStdout: true, script: 'git log --format="%ae" | head -1').trim();  
+        } 
+    }
+}
